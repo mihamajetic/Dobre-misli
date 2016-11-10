1b00327c8ee99129f64d2e80a05da5b3bb0f0e39
Miha Majetič <miha.majetic@gmail.com>
2016-11-10 10:26:23 +0100

    beleženje misli

--- Dobre misli/Default.aspx.cs
+++ Dobre misli/Default.aspx.cs
@@ -9,15 +9,29 @@ namespace Dobre_misli
 {
     public partial class Default : System.Web.UI.Page
     {
+        List<string> misli_list = new List<string>();
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+          if (Application["misli"] == null) {
+            Application["misli"] = misli_list;
+          }
+          if (Application["misli"] != null) {
+            misli_list = (List<string>)Application["misli"];
+            misli.Text = "";
+
+            foreach (string misel in misli_list) {
+              misli.Text += misel + "<br>";
+            }
+          }
+
+
         }
 
         protected void dodajMisel_Click(object sender, EventArgs e)
         {
-                    
+          misli_list.Add(misel.Text);
+          Application["misli"] = misli_list;
         }
     }
-    
+
 }
